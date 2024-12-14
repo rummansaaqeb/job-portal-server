a917e40e224810670604084178af739464faad3c
rummansaaqeb <rsaaqeb@gmail.com>
2024-12-14 15:29:44 +0600

    added api for specific job details

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -35,6 +35,13 @@ async function run() {
         app.get('/jobs', async (req, res) => {
             const cursor = jobsCollection.find();
             const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/jobs/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await jobsCollection.findOne(query);
             res.send(result);
         })
 
