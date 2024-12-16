879ebaccaeb146dfa3aaebeae6ff5da43d6d7b49
rummansaaqeb <rsaaqeb@gmail.com>
2024-12-16 21:37:08 +0600

    saved job application and got data using email

--- index.js
+++ index.js
@@ -39,16 +39,24 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/jobs/:id', async(req, res) => {
+        app.get('/jobs/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await jobsCollection.findOne(query);
             res.send(result);
-        }) 
+        })
 
 
         // job application apis
-        app.post('/job-applications', async(req, res) => {
+
+        app.get('/job-application', async (req, res) => {
+            const email = req.query.email;
+            const query = { applicant_email: email };
+            const result = await jobApplicationCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.post('/job-applications', async (req, res) => {
             const application = req.body;
             const result = await jobApplicationCollection.insertOne(application);
             res.send(result);
