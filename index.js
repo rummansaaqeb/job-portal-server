e0da37bb9c7116a5ee13ed15e5b68d478fac7df0
rummansaaqeb <rsaaqeb@gmail.com>
2024-12-24 17:55:45 +0600

    used cookie parser and set token to httpOnly cookie

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -8,6 +9,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7x5x4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -37,10 +39,15 @@ async function run() {
 
         // Auth Related APIs
 
-        app.post('/jwt', async(req, res) => {
+        app.post('/jwt', async (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, 'secret', {expiresIn: '1h'});
-            res.send(token);
+            const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '1h' });
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: false, // http://localhost:5000/
+
+
+            }).send({ success: true });
         })
 
 
