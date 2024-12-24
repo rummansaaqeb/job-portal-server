28dfc2ceec01d626eea8912f2b7ea8ea57a89250
rummansaaqeb <rsaaqeb@gmail.com>
2024-12-24 16:40:58 +0600

    added a patch request for updating status of application of applicant

--- index.js
+++ index.js
@@ -122,6 +122,19 @@ async function run() {
             res.send(result);
         })
 
+        app.patch('/job-applications/:id', async (req, res) => {
+            const id = req.params.id;
+            const data = req.body;
+            const filter = {_id: new ObjectId(id)};
+            const updatedDoc = {
+                $set: {
+                    status: data.status,
+                }
+            }
+            const result = await jobApplicationCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+
 
     } finally {
         // Ensures that the client will close when you finish/error
