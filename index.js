37b475bf18d181574c37971d4ca8bcb4ddea2b33
rummansaaqeb <rsaaqeb@gmail.com>
2024-12-25 13:21:14 +0600

    verified token and handled forbidden access

--- index.js
+++ index.js
@@ -20,24 +20,21 @@ const logger = (req, res, next) => {
 }
 
 const verifyToken = (req, res, next) => {
-    console.log('inside verifyToken middleware', req.cookies);
     const token = req.cookies?.token;
-
     if (!token) {
-        return res.status(401).send({ message: 'Unauthorized access' })
+        return res.status(401).send({ message: 'Unauthorized access' });
     }
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
-            return res.status(401).send({ message: 'Unauthorized access' })
+            return res.status(401).send({ message: "Unauthorized access" });
         }
-        //    
+        req.user = decoded;
+
         next();
     })
 
-
 }
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7x5x4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -116,6 +113,9 @@ async function run() {
             const email = req.query.email;
             const query = { applicant_email: email };
 
+            if (req.user.email !== req.query.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
 
             const result = await jobApplicationCollection.find(query).toArray();
 
